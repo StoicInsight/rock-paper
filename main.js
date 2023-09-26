6075cb158735e138ac1c87e95abca6f0a9798af1
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-09-25 22:13:26 -0400

    finished layout styling

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 console.log("Hello world")
+
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+
+let computerOptions = [ rock, paper, scissors ]
+
+let score = document.querySelector('#score')
+let trackScore = 0
+
+score.innerHTML = `${trackScore}`
+
+const runGame = () => {
+  let computerPick = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+  trackScore += 1
+  
+  score.innerHTML = `${trackScore}`
+
+  console.log(computerPick)
+  console.log(trackScore)
+}
+
+
+rock.addEventListener('click', runGame)
