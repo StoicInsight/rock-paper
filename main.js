a63d0b9729697e9b3c56732657c9efbb36e1de54
stoicinsight <65826912+StoicInsight@users.noreply.github.com>
2023-09-28 22:16:51 -0400

    document creating duplicate elements error

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-console.log("Hello world")
 
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
+let gameContainer = document.querySelector('.game-container')
+const computerChoice = document.querySelector('.computer-pick')
+const userPick = document.querySelector('.user-pick')
+const showGameWinner = document.querySelector('.winner')
+const playAgain = document.querySelector('.play-again')
+const game = document.querySelector('.game')
+
+
+// const computerOptions = [ '/images/icon-paper.svg', '/images/icon-rock.svg', '/images/icon-scissors.svg' ]
 
 let computerOptions = [ rock, paper, scissors ]
 
@@ -11,15 +19,74 @@ let trackScore = 0
 
 score.innerHTML = `${trackScore}`
 
-const runGame = () => {
-  let computerPick = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+const runGame = (e) => {
+  // const computerChoice = document.querySelector('.computer-pick')
+  const gameDecision = document.querySelector('.game-decision')
+  // const userPick = document.querySelector('.user-pick')
+  let computerPick = computerOptions[Math.floor(Math.random() * computerOptions.length)].cloneNode(true)
+  const userChoice = e.target.cloneNode(true)
+
+
+
+  console.log({
+    user: userChoice,
+    computer: computerPick
+  })
+
   trackScore += 1
+
   
+  computerPick.style.position = 'unset'
+  e.target.style.position = 'unset'
+  showGameWinner.app
+
+  computerChoice.appendChild(computerPick)
+
+  userPick.appendChild(e.target)
+
+  game.classList.add('hide')
+
+  showGameWinner.classList.remove('hide')
+
+  showGameWinner.classList.add('show')
+
   score.innerHTML = `${trackScore}`
 
-  console.log(computerPick)
-  console.log(trackScore)
+}
+
+const decideWinner = (house, yourPick) => {
+
 }
 
 
-rock.addEventListener('click', runGame)
+const restartGame = () => {
+  game.classList.remove('hide')
+
+  showGameWinner.classList.add('hide')
+
+  showGameWinner.classList.remove('show')
+
+  // computerChoice.removeChild(child)
+  // userPick.removeChild(child)
+}
+
+// const createComputerChoice = () => {
+//   let computerPick = computerOptions[Math.floor(Math.random() * computerOptions.length)]
+//   const computerChoice = document.querySelector('.computer-pick')
+
+//   const computerImg = document.createElement('img')
+//   computerImg.src = computerPick
+
+// }
+
+
+// gameContainer.forEach(el => console.log('Game container element', el))
+
+
+
+rock.addEventListener('click', (e) => runGame(e))
+scissors.addEventListener('click', (e) => runGame(e))
+paper.addEventListener('click', (e) => runGame(e))
+
+playAgain.addEventListener('click', restartGame)
+
